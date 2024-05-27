fe67d51c834cc8abcb19b1d8b58a4ec7af657594
nifaye <nicolasfaye31@gmail.com>
2024-05-27 18:10:32 +0000

    Division

---
+++ division_and_remainder/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn divide(x: i32, y: i32) -> (i32, i32) {
+    (x/y, x%y)
+}
+
+
+// fn main() {
+//     let x = 9;
+//     let y = 4;
+//     let (division, remainder) = divide(x, y);
+//     println!(
+//         "{}/{}: division = {}, remainder = {}",
+//         x, y, division, remainder
+//     );
+// }
