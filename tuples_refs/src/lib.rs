78f28a9b9225101eedd599d1c7543fc0c0257453
nifaye <nicolasfaye31@gmail.com>
2024-05-27 18:37:13 +0000

    tuples

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -17,10 +17,10 @@ pub fn last_name(student: &Student) -> String {
     student.2.to_string()
 }
 
-fn main() {
-	let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
-	println!("Student: {:?}", student);
-	println!("Student first name: {}", first_name(&student));
-	println!("Student last name: {}", last_name(&student));
-	println!("Student Id: {}", id(&student));
-}
+// fn main() {
+// 	let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
+// 	println!("Student: {:?}", student);
+// 	println!("Student first name: {}", first_name(&student));
+// 	println!("Student last name: {}", last_name(&student));
+// 	println!("Student Id: {}", id(&student));
+// }
