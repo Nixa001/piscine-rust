6f5fbb71178aa450618595ec4a10ed8914e0fd17
nifaye <nicolasfaye31@gmail.com>
2024-05-27 14:47:46 +0000

    insert

---
+++ groceries/src/lib.rs
@@ -0,0 +1,34 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+    println!("{}",vec);
+}
+
+pub fn at_index(vec: &Vec<String>, index: usize) -> String {
+    vec[index].to_string()
+}
+
+// fn main(){
+//     let mut groceries = vec![
+//             "yogurt".to_string(),
+//             "panettone".to_string(),
+//             "bread".to_string(),
+//             "cheese".to_string(),
+//         ];
+
+//         insert(&mut groceries, String::from("nuts"))
+// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let mut groceries = vec![
+            "yogurt".to_string(),
+            "panettone".to_string(),
+            "bread".to_string(),
+            "cheese".to_string(),
+        ];
+        let result = insert(&mut groceries, String::from("nuts"));
+        assert_eq!(result, 4);
+    }
+}
