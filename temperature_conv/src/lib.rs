c8a336e7bec9c1bfc23fdfe74fe5341da34708e3
nifaye <nicolasfaye31@gmail.com>
2024-05-27 13:23:05 +0000

    fix

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -3,17 +3,24 @@ pub fn fahrenheit_to_celsius(f: f64) -> f64 {
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c / (9.0 / 5.0)) + 32.0
+    (c * (9.0 / 5.0)) + 32.0
 }
+
+
+
+
+// pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+//     (c / (9.0 / 5.0)) + 32.0
+// }
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = fahrenheit_to_celsius(20.0);
-        let result2 = celsius_to_fahrenheit(0.0);
-        assert_eq!(result, -6.666666666666666);
-        // assert_eq!(result2, 32.0 );
+        let result = fahrenheit_to_celsius(-459.67);
+        let result2 = celsius_to_fahrenheit(-2.7777777777777777);
+        assert_eq!(result, -273.15 );
+        assert_eq!(result2, 27.0 );
     }
 }
