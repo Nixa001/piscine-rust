bc825edb4e4ba05e2a152a8ac5994343bf73e03a
nifaye <nicolasfaye31@gmail.com>
2024-05-28 14:37:49 +0000

    name initial

---
+++ name_initials/src/lib.rs
@@ -0,0 +1,32 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut initials: Vec<String> = vec![];
+    
+    for name in names.iter() {
+        let mut temp_string = String::new();
+        let name_split = name.split_whitespace();
+        let mut index = 0;
+        for part in name_split {
+            let first_char = part.chars().nth(0).unwrap();
+            temp_string.push(first_char);
+            temp_string.push('.');
+            if index ==0{
+                temp_string.push(' ');
+                index+=1;
+            }
+        }
+        
+        initials.push(temp_string);
+    }
+    initials
+}
+
+// fn main() {
+//     let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"];
+//     println!("{:?}", initials(names));
+// }
+
+// And its output
+
+// $ cargo run
+// ["H. P.", "S. E.", "J. L.", "B. O."]
+// $
