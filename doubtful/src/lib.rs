e0a85b1b554f772948a11e3210d85e8634cee9f3
nifaye <nicolasfaye31@gmail.com>
2024-05-28 12:54:53 +0000

    doublefult

--- doubtful/src/lib.rs
+++ doubtful/src/lib.rs
@@ -2,12 +2,12 @@ pub fn doubtful(s: &mut String) {
     s.push('?');
 }
 
-fn main() {
-    let mut s = String::from("Hello");
+// fn main() {
+//     let mut s = String::from("Hello");
 
-    println!("Before changing the string: {}", s);
+//     println!("Before changing the string: {}", s);
 
-    doubtful(&mut s);
+//     doubtful(&mut s);
 
-    println!("After changing the string: {}", s);
-}
+//     println!("After changing the string: {}", s);
+// }
