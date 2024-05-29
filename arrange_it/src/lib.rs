b33c39708ddb3f680d2ec8094486b54413c94854
nifaye <nicolasfaye31@gmail.com>
2024-05-29 18:00:13 +0000

    arragn it

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,19 +1,24 @@
 pub fn arrange_phrase(phrase: &str) -> String {
-    let mut sent_vec = vec![String::new(); phrase.split(" ").count()];
-    for (_, word) in phrase.split(" ").enumerate() {
+    let mut sent_vec = vec![String::new(); phrase.split_whitespace().count()];
+    
+    for word in phrase.split_whitespace() {
         let mut index = 0;
         let mut temp_word = String::new();
+        
         for char in word.chars() {
-            let result: Result<i32, _> = char.to_string().parse();
-            match result {
-                Ok(value) => index = value-1,
-                Err(_) => temp_word.push(char),
+            if let Some(value) = char.to_digit(10) {
+                index = value as usize - 1; 
+            } else {
+                temp_word.push(char);
             }
         }
-        sent_vec[index as usize] = temp_word;
+        
+        sent_vec[index] = temp_word;
     }
+    
     sent_vec.join(" ")
 }
+
 
 fn main() {
     let arranged_phrase = arrange_phrase("is2 Thi1s T4est 3a");
