dd885a6178dc16355c8e04d745f220ef30c40289
nifaye <nicolasfaye31@gmail.com>
2024-05-29 09:36:15 +0000

    copy

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -2,11 +2,19 @@ pub fn nbr_function(c: i32) -> (i32, f64, f64) {
     (c, (c as f64).exp(), (c.abs() as f64).ln())
 }
 pub fn str_function(a: String) -> (String, String) {
-    let exp = a.chars()
-                      .map(|ch| (ch as u8 as f64).exp().to_string())
-                      .collect::<Vec<String>>()
-                      .join(" ");
-    (a.clone(), exp)
+    let mut result = String::new();
+
+    for c in a.chars() {
+        if let Some(digit) = c.to_digit(10) {
+            let exp_value = digit as f64;
+            let exp_result = f64::exp(exp_value);
+
+            result.push_str(&exp_result.to_string());
+            result.push(' ');
+        }
+    }
+
+    (a, result.trim().to_string())
 }
 
 pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
