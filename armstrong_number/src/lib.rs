69a499de5d2f0a1980b0fab8f34475e6d4b8aace
nifaye <nicolasfaye31@gmail.com>
2024-05-29 18:21:50 +0000

    amstrong va

--- armstrong_number/src/lib.rs
+++ armstrong_number/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub fn is_armstrong_number(nb: u32) -> Option<u32>{
+    let num_str = nb.to_string();
+    let power = num_str.len() as u32;
+    let mut sum = 0;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    for c in num_str.chars() {
+        let digit = c.to_digit(10).expect(" ");
+        sum += digit.pow(power);
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // println!("{}", sum);
+    if sum == nb {
+        Some(sum)
+    } else {
+        None
     }
 }
+
+
+
+// fn main() {
+//     // is_armstrong_number(1);
+//     // is_armstrong_number(153);
+//     // is_armstrong_number(400)
+//     println!("{:?}", is_armstrong_number(0));
+//     println!("{:?}", is_armstrong_number(1));
+//     println!("{:?}", is_armstrong_number(153));
+//     println!("{:?}", is_armstrong_number(370));
+//     println!("{:?}", is_armstrong_number(371));
+//     println!("{:?}", is_armstrong_number(407));
+//     println!("{:?}", is_armstrong_number(400));
+//     println!("{:?}", is_armstrong_number(198));
+// }
+// And its output:
+
+// $ cargo run
+// Some(0)
+// Some(1)
+// Some(153)
+// Some(370)
+// Some(371)
+// Some(407)
+// None
+// None
+// $  for digit in num_str.chars().map(|c| c.to_digit(10).unwrap()) {
+       
