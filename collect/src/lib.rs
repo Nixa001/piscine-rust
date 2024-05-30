5c37d2dd8149f2d6a82ba50eb8c5b07ce70289b2
nifaye <nicolasfaye31@gmail.com>
2024-05-30 22:05:42 +0000

    Collect

--- collect/src/lib.rs
+++ collect/src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn bubble_sort(vec: &mut Vec<i32>) {
+    let n = vec.len();
+    for i in 0..n {
+        // Track if any swapping happens
+        let mut swapped = false;
+        for j in 0..(n - i - 1) {
+            if vec[j] > vec[j + 1] {
+                vec.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+        // If no elements were swapped, the list is sorted
+        if !swapped {
+            break;
+        }
     }
 }
+
+// fn main() {
+//     let ref mut v = vec![3, 2, 4, 5, 1, 7];
+//     let mut b = v.clone();
+//     bubble_sort(v);
+//     println!("{:?}", v);
+
+//     b.sort();
+//     println!("{:?}", b);
+// }
