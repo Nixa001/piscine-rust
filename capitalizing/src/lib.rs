8fde9e593018fed9d978af02f558abf8580a4041
nifaye <nicolasfaye31@gmail.com>
2024-05-30 17:07:55 +0000

    capitazing

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn capitalize_first(input: &str) -> String {
+let mut str = String::new();
+let mut is_first = true;
+for char in input.chars(){
+    if is_first{
+       str.push(char.to_ascii_uppercase());
+       is_first = false;
+    }else{
+       str.push(char);
     }
 }
+str.to_string()
+}
+
+pub fn title_case(input: &str) -> String {
+    let mut str = String::new();
+    let mut is_first = true;
+    for char in input.chars(){
+        if is_first{
+        str.push(char.to_ascii_uppercase());
+        is_first = false;
+        }else{
+        str.push(char);
+        }
+        if char == ' '{
+            is_first = true;
+        }
+    }
+    str.to_string()
+}
+
+pub fn change_case(input: &str) -> String {
+    let mut str = String::new();
+    for char in input.chars(){
+        if char.is_ascii_lowercase(){
+        str.push(char.to_ascii_uppercase());
+        }else{
+            str.push(char.to_ascii_lowercase());
+        }
+    }
+    str.to_string()
+}
+
+
+fn main() {
+    println!("{}", capitalize_first("joe is missing"));
+    println!("{}", title_case("jill is leaving A"));
+    println!("{}",change_case("heLLo THere"));
+}
+// And its output
+
+// $ cargo run
+// Joe is missing
+// Jill Is Leaving A
+// HEllO thERE
+// $
