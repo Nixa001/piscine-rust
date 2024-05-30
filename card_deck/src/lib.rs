a3dcab381ad3238e687d021dd4fd294dff0c6aaa
nifaye <nicolasfaye31@gmail.com>
2024-05-30 12:49:45 +0000

    card

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,63 +1,88 @@
-// extern crate rand;
-// use rand::Rng;
+use rand::Rng; 
 
 #[derive(Debug, PartialEq)]
 pub enum Suit {
-    Heart,
-    Diamond,
-    Spade,
-    Club,
+    Heart,   
+    Diamond, 
+    Spade,   
+    Club,    
 }
-
 impl Suit {
+    
     pub fn random() -> Suit {
-        let mut value = rand::thread_rng().gen_range(1, 5);
+        let mut rng = rand::thread_rng();
+        let value = rng.gen_range(1, 5); 
         Suit::translate(value)
     }
-
+    
     pub fn translate(value: u8) -> Suit {
         match value {
-            1 => Suit::Heart,
-            2 => Suit::Diamond,
-            3 => Suit::Spade,
-            4 => Suit::Club,
-            _ => panic!("Invalid suit value"),
+            1 => Suit::Heart,   
+            2 => Suit::Diamond, 
+            3 => Suit::Spade,   
+            _ => Suit::Club,    
         }
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub enum Rank {
-    Ace,
-    King,
-    Queen,
-    Jack,
-    Number(u8),
+    Ace,        
+    King,       
+    Queen,      
+    Jack,       
+    Number(u8), 
 }
-
 impl Rank {
-    pub fn random() -> Rank {
-        let mut value = rand::thread_rng().gen_range(1, 14);
+        pub fn random() -> Rank {
+        let mut rng = rand::thread_rng();
+        let value = rng.gen_range(1, 14); 
         Rank::translate(value)
     }
-
+    
     pub fn translate(value: u8) -> Rank {
         match value {
-            1 => Rank::Ace,
-            2..=10 => Rank::Number(value),
-            11 => Rank::Jack,
-            12 => Rank::Queen,
-            13 => Rank::King,
-            _ => panic!("Invalid rank value"),
+            1 => Rank::Ace,        
+            11 => Rank::Jack,      
+            12 => Rank::Queen,     
+            13 => Rank::King,      
+            n => Rank::Number(n),  
         }
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
 pub fn winner_card(card: &Card) -> bool {
-    card.suit == Suit::Spade && card.rank == Rank::Ace
-}
+    let winner = Card {
+        suit: Suit::Spade, 
+        rank: Rank::Ace,   
+    };
+    *card == winner 
+} 
+
+ 
+
+// Main function for testing
+// fn main() {
+// 	let your_card = Card {
+// 		rank: Rank::random(),
+// 		suit: Suit::random(),
+// 	};
+
+// 	println!("Your card is {:?}", your_card);
+
+// 	// Now if the card is an Ace of Spades print "You are the winner"
+// 	if card_deck::winner_card(&your_card) {
+// 		println!("You are the winner!");
+// 	}
+// }
+// And its output
+
+// $ cargo run
+// Your card is Card { suit: Club, rank: Ace }
+// $
