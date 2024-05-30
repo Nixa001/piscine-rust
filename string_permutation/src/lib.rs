94512ac760937f577cbfa1dd87109ad539d53e78
nifaye <nicolasfaye31@gmail.com>
2024-05-30 22:02:18 +0000

    string permut

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+
+pub fn is_permutation(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() {
+        return false;
+    }
+
+    fn char_count(s: &str) -> HashMap<char, usize> {
+        let mut count = HashMap::new();
+        for c in s.chars() {
+            *count.entry(c).or_insert(0) += 1;
+        }
+        count
+    }
+
+    let count1 = char_count(s1);
+    let count2 = char_count(s2);
+
+    count1 == count2
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn main() {
+    let word = "thought";
+    let word1 = "thougth";
+    println!(
+        "Is `{}` a permutation of `{}`? = {}",
+        word,
+        word1,
+        is_permutation(word, word1)
+    );
 }
