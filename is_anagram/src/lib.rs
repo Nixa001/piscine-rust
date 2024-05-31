49cdd383cc71b34b1dceb2c8510c45f31a3ad95b
nifaye <nicolasfaye31@gmail.com>
2024-05-31 22:38:14 +0000

    is anagr

--- is_anagram/src/lib.rs
+++ is_anagram/src/lib.rs
@@ -5,21 +5,9 @@ pub fn is_anagram(s1: &str, s2: &str) -> bool {
         return false;
     }
 
-    let mut char_count: HashMap<char, usize> = HashMap::new();
-    for c in s1.chars() {
-        *char_count.entry(c).or_insert(0) += 1;
-    }
-
-    for c in s2.chars() {
-        if let Some(count) = char_count.get_mut(&c) {
-            *count -= 1;
-            if *count == 0 {
-                char_count.remove(&c);
-            }
-        } else {
-            return false;
-        }
-    }
-
-    char_count.is_empty()
+    let mut chars1: Vec<char> = s1.chars().collect();
+    let mut chars2: Vec<char> = s2.chars().collect();
+    chars1.sort_unstable();
+    chars2.sort_unstable();
+    chars1.into_iter().collect::<String>() == chars2.into_iter().collect::<String>()
 }
