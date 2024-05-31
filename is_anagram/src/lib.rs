f3f7bc9130c40bd005cd01d49b67ac7467693293
nifaye <nicolasfaye31@gmail.com>
2024-05-31 20:01:36 +0000

    is anagram

--- is_anagram/src/lib.rs
+++ is_anagram/src/lib.rs
@@ -1,10 +1,10 @@
-pub fn is_anagram(s1: &str, s2: &str) -> bool {
+pub fn is_anagram(s1: &str, s2: &str) -> bool {    
     if s1.len() != s2.len() {
         return false;
     }
 
-    let mut s1_chars = s1.chars().collect();
-    let mut s2_chars = s2.chars().collect();
+    let mut s1_chars: Vec<char> = s1.chars().collect();
+    let mut s2_chars: Vec<char> = s2.chars().collect();
 
     s1_chars.sort_unstable();
     s2_chars.sort_unstable();
