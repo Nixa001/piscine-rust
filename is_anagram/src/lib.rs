55dd4245eea2f3486d8b497486cd44e578214cf6
nifaye <nicolasfaye31@gmail.com>
2024-05-31 20:00:27 +0000

    is anagram

--- is_anagram/src/lib.rs
+++ is_anagram/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn is_anagram(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() {
+        return false;
     }
+
+    let mut s1_chars = s1.chars().collect();
+    let mut s2_chars = s2.chars().collect();
+
+    s1_chars.sort_unstable();
+    s2_chars.sort_unstable();
+
+    s1_chars == s2_chars
 }
