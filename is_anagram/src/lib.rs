7a9d52a7357451e298cdc588efed1c912573db06
nifaye <nicolasfaye31@gmail.com>
2024-05-31 22:17:35 +0000

    is anagram

--- is_anagram/src/lib.rs
+++ is_anagram/src/lib.rs
@@ -1,13 +1,14 @@
-pub fn is_anagram(s1: &str, s2: &str) -> bool {    
-    if s1.len() != s2.len() {
+pub fn is_anagram(s1: &str, s2: &str) -> bool {
+    // Check if both strings have the same length
+    if s1.len()!= s2.len() {
         return false;
     }
 
-    let mut s1_chars: Vec<char> = s1.chars().collect();
-    let mut s2_chars: Vec<char> = s2.chars().collect();
+    let mut chars_s1: Vec<char> = s1.chars().collect();
+    let mut chars_s2: Vec<char> = s2.chars().collect();
 
-    s1_chars.sort_unstable();
-    s2_chars.sort_unstable();
+    chars_s1.sort_unstable();
+    chars_s2.sort_unstable();
 
-    s1_chars == s2_chars
+    chars_s1 == chars_s2
 }
