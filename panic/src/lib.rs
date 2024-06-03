eb7c41b23bcdf63badb6585e425ba8f42c2a7ee6
nifaye <nicolasfaye31@gmail.com>
2024-06-03 11:45:05 +0000

    panic

---
+++ panic/src/lib.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+
+    pub fn open_file(s: &str) -> File {
+        File::open(s).expect("File not found")
+    }
+
+
+
+// fn main() {
+//     let filename = "created.txt";
+//     File::create(filename).unwrap();
+
+//     let a = open_file(filename);
+//     println!("{:?}", a);
+    
+//     fs::remove_file(filename).unwrap();
+
+//     //It must panic
+//     let b = open_file(filename);
+// }
+
+// And its output:
+
+// $ cargo run
+// File { fd: 3, path: "panic/a.txt", read: true, write: false }
+// thread 'main' panicked at 'File not found'
+// $
