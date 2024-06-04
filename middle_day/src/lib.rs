b57224f7a3988eef70acc1d393c127923b695db3
nifaye <nicolasfaye31@gmail.com>
2024-06-04 23:30:06 +0000

    midday

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -4,10 +4,10 @@ use chrono::{Datelike, NaiveDate, Weekday as wd};
 pub fn middle_day(year: i32) -> Option<wd> {
     let last_day = NaiveDate::from_ymd_opt(year, 12, 31)?;
     let days_in_y = last_day.ordinal();
-    if days_in_y % 2 != 0 {
+    if days_in_y % 2!= 0 {
         NaiveDate::from_ymd_opt(year, 12, 31)?
-            .checked_add_signed(chrono::Duration::days(days_in_y as i64 / 2))
-            .map(|d| d.Weekday())
+           .checked_add_signed(chrono::Duration::days(days_in_y as i64 / 2))
+           .map(|d| d.weekday())
     } else {
         None
     }
@@ -16,9 +16,3 @@ pub fn middle_day(year: i32) -> Option<wd> {
 fn main() {
     println!("{:?}", middle_day(1022).unwrap());
 }
-
-// And its output:
-
-// $ cargo run
-// Tue
-// $
