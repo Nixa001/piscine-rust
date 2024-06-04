0605a44f8ae70d6677bfb40158f9bd53184d5e2a
nifaye <nicolasfaye31@gmail.com>
2024-06-04 23:46:32 +0000

    midday

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,18 +1,12 @@
 extern crate chrono;
-use chrono::{Datelike, NaiveDate, Weekday as wd};
-
+pub use chrono::{Datelike, NaiveDate, Weekday as wd};
 pub fn middle_day(year: i32) -> Option<wd> {
-    let last_day = NaiveDate::from_ymd_opt(year, 12, 31)?;
-    let days_in_y = last_day.ordinal();
-    if days_in_y % 2!= 0 {
-        NaiveDate::from_ymd_opt(year, 12, 31)?
-           .checked_add_signed(chrono::Duration::days(days_in_y as i64 / 2))
-           .map(|d| d.weekday())
-    } else {
-        None
+    if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) {
+        return None;
     }
-}
-
-fn main() {
-    println!("{:?}", middle_day(1022).unwrap());
+    let date = NaiveDate::from_ymd_opt(year, 7, 2);
+    match date {
+        Some(d) => Some(d.weekday()),
+        None => None,
+    }
 }
