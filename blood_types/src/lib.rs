3d293816c3cf6d6d7e2d27969bf951f03650853b
nifaye <nicolasfaye31@gmail.com>
2024-06-12 19:44:21 +0000

    blood_types

---
+++ blood_types/src/lib.rs
@@ -0,0 +1,66 @@
+#[derive(Debug, PartialEq, Eq, Clone, PartialOrd, Ord)]
+pub enum Antigen {
+    A,
+    AB,
+    B,
+    O,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+enum RhFactor {
+    Positive,
+    Negative,
+}
+
+#[derive(PartialEq, Eq, PartialOrd)]
+pub struct BloodType {
+    pub antigen: Antigen,
+    pub rh_factor: RhFactor,
+}
+
+use std::fmt;
+use std::str::FromStr;
+
+impl FromStr for Antigen {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" => Ok(Antigen::A),
+            "AB" => Ok(Antigen::AB),
+            "B" => Ok(Antigen::B),
+            "O" => Ok(Antigen::O),
+            _ => Err(()),
+        }
+    }
+}
+
+impl FromStr for RhFactor {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "+" => Ok(RhFactor::Positive),
+            "-" => Ok(RhFactor::Negative),
+            _ => Err(()),
+        }
+    }
+}
+
+impl FromStr for BloodType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < 2 {
+            return Err(());
+        }
+
+        let antigen_part = &s[0..s.len() - 1];
+        let rh_part = &s[s.len() - 1..];
+
+        let antigen = antigen_part.parse::<Antigen>()?;
+        let rh_factor = rh_part.parse::<RhFactor>()?;
+
+        Ok(BloodType { antigen, rh_factor })
+    }
+}
