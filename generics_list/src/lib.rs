0845a9f1b1de3f0528b4a4ae67b6232732b9147c
nifaye <nicolasfaye31@gmail.com>
2024-06-12 12:01:35 +0000

    generics_list

--- generics_list/src/lib.rs
+++ generics_list/src/lib.rs
@@ -1,3 +1,5 @@
+// Définition des structures et implémentations
+
 #[derive(Clone, Debug)]
 pub struct List<T> {
     pub head: Option<Node<T>>,
@@ -10,31 +12,29 @@ pub struct Node<T> {
 }
 
 impl<T> List<T> {
-    pub fn new() -> Self {
+    pub fn new() -> List<T> {
         List { head: None }
     }
 
     pub fn push(&mut self, value: T) {
-        let new_node = Box::new(Node {
+        let new_node = Node {
             value,
-            next: self.head.take(),
-        });
+            next: self.head.take().map(box::new),
+        };
         self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|node| {
-            self.head = node.next;
-            node.value
-        })
+    pub fn pop(&mut self) {
+        if let Some(node) = self.head.take() {
+            self.head = node.next.map(|boxed_node| *boxed_node);
+        }
     }
-
     pub fn len(&self) -> usize {
         let mut count = 0;
-        let mut current = self.head.as_ref();
-        while let Some(node) = current {
+        let mut current_node = self.head.as_ref().map(|n| n);
+        while let Some(node) = current_node {
             count += 1;
-            current = node.next.as_ref();
+            current_node = node.next.as_ref().map(|boxed_node| &**boxed_node);
         }
         count
     }
